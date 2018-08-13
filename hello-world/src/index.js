21e00de7c493184adfca578b610f56590de65d47
jessieshao <505133853@qq.com>
2018-08-13 13:08:19 +0800

    框架基本完成

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './App.css';
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-  );
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.states = {
+            newTodo: 'test',
+            todoList: [
+                {id: 1, title: '第一个待办'}
+            ]
+        }
+    }
+    render(){
+        let todos = this.states.todoList.map((item,index)=>{
+            return <li>{item.title}</li>
+        })
+
+        return (
+            <div className = "App">
+                 <h1>我的待办</h1>
+                 <div className="inputWrapper">
+                    <input type="text" value={this.state.newTodo}/>
+                 </div>
+                 <ol>
+                    {todos}
+                 </ol>
+            </div>
+        )
+    }
+}
